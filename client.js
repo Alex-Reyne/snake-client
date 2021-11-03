7867106949f97e456b63d48d096f83e4814b8928
Alex-Reyne <creativereyne@gmail.com>
2021-11-03 12:27:14 -0600

    add name to snake on connection

--- client.js
+++ client.js
@@ -14,6 +14,10 @@ const connect = function () {
     console.log('data: ', data);
   });
 
+  conn.on('connect', () => {
+    conn.write('Name: ALR');
+  });
+
   conn.setEncoding("utf8");
 
   return conn;
