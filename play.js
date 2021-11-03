fccd9afdb5266b592e6c28c5a3f779a88b2693c4
Alex-Reyne <creativereyne@gmail.com>
2021-11-03 13:18:54 -0600

    add userInput callback to handle keypresses.

--- play.js
+++ play.js
@@ -1,6 +1,25 @@
 const net = require('net');
 const { connect } = require('./client.js');
 
+const setupInput = function (callback) {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on("data", callback);
+
+  return stdin;
+}
+
+const handleUserInput = function (key) {
+  
+  if (key === '\u0003') {
+    process.exit();
+  }
+
+};
+
 
 console.log(`connecting...`);
 connect();
+setupInput(handleUserInput);
